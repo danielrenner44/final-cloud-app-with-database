8e82c8ba02b3127e7cd56bb9b02b8b5d0302acd2
danielrenner44 <danielrenner44@gmail.com>
2022-07-04 07:18:57 +0000

    all

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -142,4 +142,4 @@ class Choice(models.Model):
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
-    Other fields and methods you would like to design
+#Other fields and methods you would like to design
